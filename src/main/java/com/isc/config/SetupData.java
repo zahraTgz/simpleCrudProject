f144f4974d285f39e84ee0f01682cdf518c61d4c
zahra <zara.taghizadeh@gmail.com>
2023-04-25 15:16:14 +0330

    refactor SpringSecurityConfig

--- src/main/java/com/isc/config/SetupData.java
+++ src/main/java/com/isc/config/SetupData.java
@@ -1,9 +1,7 @@
 package com.isc.config;
 
-import com.isc.entity.Account;
 import com.isc.entity.AppUser;
 import com.isc.entity.AppUserRole;
-import com.isc.repository.AccountRepository;
 import com.isc.repository.UserRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
@@ -13,17 +11,13 @@ public class SetupData {
 
     private final UserRepository userRepository;
 
-    private final AccountRepository accountRepository;
-
-    public SetupData(UserRepository userRepository, AccountRepository accountRepository) {
+    public SetupData(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.accountRepository = accountRepository;
     }
 
     @PostConstruct
     public void init() {
         initUsers();
-        initAccount();
     }
 
     private void initUsers() {
@@ -41,13 +35,6 @@ public class SetupData {
                 .role(AppUserRole.USER)
                 .build();
         userRepository.save(user);
-    }
-
-    private void initAccount() {
-        Account firstAccount = Account.builder().name("zahra").email("z_tgz@isc.com").age(22).build();
-        accountRepository.save(firstAccount);
-        Account secondAccount = Account.builder().name("elahe").email("e_tgz@isc.com").age(25).build();
-        accountRepository.save(secondAccount);
     }
 
 }
--- src/main/java/com/isc/config/SpringSecurityConfig.java
+++ src/main/java/com/isc/config/SpringSecurityConfig.java
@@ -1,13 +1,9 @@
 package com.isc.config;
 
-import com.isc.service.CustomAuthenticationProvider;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,23 +12,6 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
-
-
-    @Qualifier("MyUserDetailsService")
-    UserDetailsService memberDetailsService;
-
-    private final CustomAuthenticationProvider customAuthenticationProvider;
-
-    public SpringSecurityConfig(CustomAuthenticationProvider customAuthenticationProvider) {
-        this.customAuthenticationProvider = customAuthenticationProvider;
-    }
-
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .authenticationProvider(customAuthenticationProvider)
-                .userDetailsService(memberDetailsService)
-                .passwordEncoder(passwordEncoder());
-    }
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
@@ -44,11 +23,9 @@ public class SpringSecurityConfig {
                         .permitAll()
                         .anyRequest()
                         .authenticated())
-                .formLogin();
+                .formLogin().and()
+                .httpBasic();
 
-        httpSecurity.headers()
-                .frameOptions()
-                .sameOrigin();
         return httpSecurity.build();
     }
 
@@ -56,6 +33,4 @@ public class SpringSecurityConfig {
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-
 }
