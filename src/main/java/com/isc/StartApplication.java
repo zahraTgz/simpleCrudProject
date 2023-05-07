9dafc28dc0df85bdc9c40031b6de81fd9181c13b
zahra <zara.taghizadeh@gmail.com>
2023-05-07 10:55:11 +0330

    add SecurityScheme in swagger

--- src/main/java/com/isc/StartApplication.java
+++ src/main/java/com/isc/StartApplication.java
@@ -1,7 +1,11 @@
 package com.isc;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +15,12 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource("classpath:dec_fa.properties")
 @SpringBootApplication
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Crud API", version = "1.0", description = "Simple SpringBoot App"))
+@SecurityScheme(type = SecuritySchemeType.APIKEY, name = "Authorization", in = SecuritySchemeIn.HEADER)
+@OpenAPIDefinition(info = @Info(
+        title = "Crud API",
+        version = "1.0",
+        description = "Simple SpringBoot App"),
+        security = {@SecurityRequirement(name = "Authorization")})
 public class StartApplication {
 
     public static void main(String[] args) {
